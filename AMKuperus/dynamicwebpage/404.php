0ec7054a7810d237b1c1661acd9b150dfe86dfa6
AMKuperus <jezeewolde@gmail.com>
2017-02-09 14:28:27 +0100

    Upoad all files done so far

---
+++ AMKuperus/dynamicwebpage/404.php
@@ -0,0 +1,5 @@
+<?php
+  echo "<h2>404 Page not found...</h2>";
+  echo "<p>Ooops... there seems to be something missing in here</p>";
+  echo "<a href='?p=home.php'>Go back home</a>";
+?>
---
+++ AMKuperus/flags/flags.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<head>
+  <title>Flags</title>
+  <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+  <h1>Flags</h1>
+  <form action="flags.php" method="GET">
+  <?php
+    include 'flags.inc.php';
+  ?>
+</form>
+</body>
+</html>
