446839603d69bc247ae6281e738f7b435f256af8
MrJurn <jadouwesisema@gmail.com>
2017-02-09 14:26:53 +0100

    Update bmi.php

--- JURN/BMI-Calculator/bmi.php
+++ JURN/BMI-Calculator/bmi.php
@@ -20,7 +20,5 @@ $bmi = $weight / ($height * $height);
 print("The body mass index is $bmi");
  
 ?> 
-</br>
-<a href="http://localhost/ITVITAE/BMI-Calculator/bmi.html">back to BMI calculator<a/>
 </body> 
 </html>
