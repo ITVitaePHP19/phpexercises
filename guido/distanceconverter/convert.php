a4feac5f173072d372604e803b341e8a33b97626
guidoleijten <guido.leijten@itvitaelearning.nl>
2017-02-13 15:03:53 +0100

    Distance converter

--- guido/distanceconverter/convert.php
+++ guido/distanceconverter/convert.php
@@ -3,18 +3,17 @@ if (isset($_POST['convert'])) {
 	convertDistance();
 }
 
-
 function convertDistance() {
 		$input = $_POST['in'];
-		$kilometers = $input * 0.621371192;
-		$miles = $input * 1.609344;
+		$kilometers = round($input * 0.621371192, 1);
+		$miles = round($input * 1.609344, 1);
 			if ($_POST['distance'] == "kilometers") {
 	// posted in name 'distance' radio button and value of form == "kilometers"
-			echo $input . "<p>miles is</p>" . $kilometers . "<p>kilometer</p>";
+			echo "$input miles is $kilometers kilometer";
 		}
 		elseif ($_POST['distance'] == "miles") {
 	// posted in name 'distance' radio button and value of form == "miles"
-			echo $input . "<p>kilometer is</p>" . $miles . "<p>miles</p>";;
+			echo "$input kilometer is $miles miles";
 		}
 }
 ?>
