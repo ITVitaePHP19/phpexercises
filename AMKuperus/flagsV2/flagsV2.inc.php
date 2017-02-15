9e21813883fc67a49800f3b9654ec41f30fc8356
AMKuperus <jezeewolde@gmail.com>
2017-02-15 11:08:55 +0100

    Flags V2.0
    Udated quiz files. Still work in progress.

--- AMKuperus/flagsV2/flagsV2.inc.php
+++ AMKuperus/flagsV2/flagsV2.inc.php
@@ -4,11 +4,57 @@ $dir = 'images/';
 
 //Creating a array with all the images in the folder.
 $files = (glob("$dir*.{png,jpg,gif}", GLOB_BRACE));
-$i = 1;
-foreach($files as $f) {
-  echo '<figure class="flexin"><img src=' . $f . ' width=150 height=100>
-        <figcaption>' . $i . '. ' . createName($f, $dir) . '</figure>';
-  $i++;
+
+//Determine what to show on what $_GET['do']-content.
+if(isset($_GET['do'])) {
+  $do = $_GET['do'];
+  switch ($do) {
+    case "flags":
+      allFlags($files, $dir);
+      break;
+    case "quiz":
+      flagQuiz($files, $dir);
+      break;
+    default:
+      echo "Something went wrong here, <a href=''>go back</a>.";
+  }
+}
+
+//Shows all the flags on the screen in a flexbox.
+function allFlags($files, $dir){
+  $i = 1;
+  foreach($files as $f) {
+    echo '<figure class="flexin"><img src=' . $f . ' width=150 height=100>
+          <figcaption>' . $i . '. ' . createName($f, $dir) . '</figure>';
+    $i++;
+  }
+}
+
+//Quiz-function that shows a flag and 4 names, when a name is submitted compare
+//submission with the name of the flag (countryname) and if correct say so, and
+//give the next quiz-item.
+function flagQuiz($files, $dir) {
+  //Create 2 empty arrays to store the flags and the names seperate so we can
+  //remove flags when done and have all flagnames as random-possibilities.
+  $flags = [];
+  $names = [];
+  //Filling the 2 new arrays with flags and names.
+  foreach($files as $f) {
+    array_push($flags, $f);
+    array_push($names, createName($f, $dir));
+  }
+  echo 'Library holds ' .  count($flags) . ' flags currently.';
+
+  while(count($flags) > 0) {
+    //TODO show a flag
+    //TODO remove that flag from array
+    //TODO get 3 random other names
+    //TODO show 4 names with radiobuttons
+    //TODO submitbutton with a action to check the submission true/false
+  }
+
+  echo "<br>";
+  echo mt_rand(0, count($flags));
 }
 
 //Create a name for the picture by stripping extension and adding a capital.
--- AMKuperus/flagsV2/flagsV2.php
+++ AMKuperus/flagsV2/flagsV2.php
@@ -5,6 +5,10 @@
 </head>
 <body>
   <h1>Flags V2.0</h2>
+    <nav>
+      <a href="?do=flags">Flags</a>
+      <a href="?do=quiz">Quiz</a>
+    </nav>
     <p>The improved version. This version accepts .jpg .gif and .png files. To add a file to flags safe the file in the images/-directory as coun_try.jpg/gif/png and the program will automatically on refresh extend the library with you're image.</p>
     <form action="" method="GET">
       <div class="flex">
