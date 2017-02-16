eb3beda33927a1be9aaaf39523ed6b93e4ffce05
AMKuperus <jezeewolde@gmail.com>
2017-02-16 14:55:44 +0100

    Flags V2 Beta
    Updates on the quiz.

--- AMKuperus/flagsV2/quizbeta.inc.php
+++ AMKuperus/flagsV2/quizbeta.inc.php
@@ -24,16 +24,45 @@
     $randomFlags = $_SESSION['randomFlags'];
     echo ">>>>>>>>>>>>>>>>>This is 2nd part of the loop<<<<<<<<<<<<<<<<<<<";
     echo count($randomFlags) . '<hr>';
-    $count = count($answers);
-    echo $count;
+
+    //Post the answer if there is POST
+    if(isset($_POST['answer'])) {
+      array_push($answers, $_POST['answer']);
+      $_SESSION['answers'] = $answers;
+      print_r($answers);
+    }
+
     //Start the quiz
     if(count($answers) < count($randomFlags)) {
-      array_push($answers, "tst");
+      //Setup counter
+      $count = count($answers);
+      echo $count;//TODO count wont work correctly with combination of the array why>?
+
+      //Show the flag for the quiz.
+      echo '<figure><img src="' . $randomFlags[$count] . '" width="600" height="400">
+            </figure>';
+      //array_push($answers, $randomFlags[$count]);
+      createAnswers($randomFlags, $count, $dir);
+
     }
 
     echo "<br>answers is set";
-    $_SESSION['answers'] = $answers;
-    print_r($answers);
+    echo "<hr>";
+    print_r($randomFlags);
+  }
+
+  //Create 3 random answers and the correct answer.
+  //Prep them with HTML-tags
+  function createAnswers($array, $count, $dir) {
+    //Create 3 random keys
+    $a = array_rand($array, 3);
+    //Add the 4th (correct answer) key
+    array_push($a, $count);
+    //Grab each key and change it to a prepped html-line
+    foreach($a as $q) {
+      echo '<input type="radio" value="' . $array[$q] . '" name="answer">' .
+      createName($array[$q], $dir) . '</option>';
+    }
   }
 ?>
 <input type="submit" value="Submit">
