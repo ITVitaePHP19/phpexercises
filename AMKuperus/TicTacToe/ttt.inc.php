dbb3e9db4fa467e5400bd7e94db471165bcf1645
AMKuperus <jezeewolde@gmail.com>
2017-02-24 21:52:45 +0100

    TicTacToe V0.9
    + Some small fixes
    + Added a stylesheet for layout
    - Removed some obsolute files

--- AMKuperus/TicTacToe/ttt.inc.php
+++ AMKuperus/TicTacToe/ttt.inc.php
@@ -2,7 +2,7 @@
 ##############################################################################
 ##################TicTacToe by @author : AMKuperus############################
 ####Copyleft,only to be used for non-profit and always mention the author.####
-##########################Version 0.8Beta--Feb.2017###########################
+##########################Version 0.9Beta--Feb.2017###########################
 ##############################################################################
 
 echo '<form action="" method="POST">';
@@ -51,14 +51,14 @@ echo '<form action="" method="POST">';
       $k = array_search($pos, $field);
       //Define who's turn it is (x/o)
       if($turn % 2 == 0) {//If true %turn is even if false $turn is uneven
-        echo "<hr>X>>>Even is playing<br>Turn: $turn<hr>\n";
+        echo "<hr><h2>X is playing</h2><p>Turn: $turn</p><hr>\n";
         //Add to the $o[] and push to the $_SESSION['o'] and $field[]
         array_push($o, $pos);
         isWinner($o);
         $field[$k] = "O";
         $_SESSION['o'] = $o;
       } else {
-        echo "<hr>O>>>Odd is playing<br>Turn: $turn<hr>\n";
+        echo "<hr><h2>O is playing</h2><p>Turn: $turn</p><hr>\n";
         //Add to the $x[] and push to the $_SESSION['x'] and $field[]
         array_push($x, $pos);
         isWinner($x);
@@ -68,14 +68,8 @@ echo '<form action="" method="POST">';
       $_SESSION["field"] = $field;
       $turn++;
       $_SESSION['turn'] = $turn;
-      //TODO Some stuff only for testingREMOVE WHEN DONE>>>>
-      //echo '<hr>Array size: ' . count($o) . ' O>>>ARRAY>>>';
-      //print_r($o);
-      //echo '<hr>Array size: ' . count($x) . ' X>>>ARRAY>>>';
-      //print_r($x);
-      //echo '<hr>Array size: ' . count($field) . ' FIELD ARRAY>>';
-      //print_r($field);
-      //echo "<br>";
+    } else {
+      echo "<h2>X starts the game</h2>";
     }
     //Setup the field in 3x3 setting with a table
     echo "<table>";
