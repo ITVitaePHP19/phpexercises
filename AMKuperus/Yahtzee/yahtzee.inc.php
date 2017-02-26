dbc3df575e4dc12adf8b11dc1112c60eb215ac49
AMKuperus <jezeewolde@gmail.com>
2017-02-26 12:15:26 +0100

    Yahtzee V0.2BETA
    + Cleaned up previous work
    + Created swichtstatement for creating number of players in the game.
    + Tested setting up number of players
    --TODO - Let each player play the game - Write the game.

--- AMKuperus/Yahtzee/yahtzee.inc.php
+++ AMKuperus/Yahtzee/yahtzee.inc.php
@@ -2,9 +2,9 @@
 ################################################################################
 ##########################Yahtzee by @author AMKuperus##########################
 #####Copyleft,only to be used for non-profit and always mention the author.#####
-############################Version 0.1-BETA-Feb.2017###########################
+############################Version 0.2-BETA-Feb.2017###########################
 ################################################################################
-//TODO CREATE Setup start off the game->select #players and setup a game from there
+##DONE//TODO CREATE Setup start off the game->select #players and setup a game from there
 //TODO CREATE Play the game, if there is players
 //TODO functions to apply the rules of Yahtzee
 //TODO Let the game run max number round, keep track of scores of each players
@@ -14,9 +14,58 @@
 //TODO safe players "turns" in a array per player(multidimensional?)
 //TODO make the score-thingy in a visible scorebooklook apear per player wneh player plays
 //TODO keep track of the score via the scorebook, which puts it into an array
-echo "TESTING $players";
-
 //Dice <radiobutton>if isset rollDice else nothing
+
+  //Reset the game
+  if (isset($_POST['reset'])) {
+    $_SESSION = [];
+  }
+
+  //Ask for the number of players and setup the game
+  if(!isset($_POST['players'])) {
+    //Show a form to choose number of players
+    echo '<p>Choose the number off players</p>
+            <select name="players" required>
+              <option value=""></option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              </select>
+            <input type="submit" value="Start">';
+  } elseif(!isset($_SESSION['players'])) {
+    //Setup players
+    $players = $_POST['players'];
+    switch($players) {
+      case 2:
+        $player1 = [];
+        $player2 = [];
+        $_SESSION['players'] = [$player1, $player2];
+        break;
+      case 3:
+        $player1 = [];
+        $player2 = [];
+        $player3 = [];
+        $_SESSION['players'] = [$player1, $player2, $player3];
+        break;
+      case 4:
+        $player1 = [];
+        $player2 = [];
+        $player3 = [];
+        $player4 = [];
+        $_SESSION['players'] = [$player1, $player2, $player3, $player4];
+        break;
+    }
+    //echo '<input type="submit" value="Start">';
+    print_r($_SESSION['players']);
+  } else {
+    //Start a game
+    $players = $_SESSION['players'];
+    $nop = count($players);//NumberOffPlayers
+    $_SESSION['nop'] = $nop;
+    echo "Number off players: $nop<hr>\n";
+    print_r($players);
+    //Select player to play and play
+  }
 
   //Roll a dice, $dice is the dice to roll
   function rollDice($dice) {
--- AMKuperus/Yahtzee/yahtzee.php
+++ AMKuperus/Yahtzee/yahtzee.php
@@ -8,28 +8,9 @@
 </head>
 <body>
   <h1>Play Yahtzee</h1>
+  <form action="" method="POST">
     <?php
-    if(isset($_POST['reset'])) {
-      $_SESSION = [];
-    }
-    echo '<form action="" method="POST">';
-      if(!isset($_POST['players'])) {
-        echo '<p>Choose the number off players</p>
-                <select name="players" required>
-                  <option value=""></option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                  <option value="4">4</option>
-                  </select>
-                <input type="submit" value="Start">
-              </form>';
-      } elseif($_SERVER['REQUEST_METHOD'] == 'POST') {
-          $players = $_POST['players'];
-          $_SESSION['players'] = $players;
-          echo $players;
-          include 'yahtzee.inc.php';
-          echo '<input type="submit" value="Play">';
-      }
+      include 'yahtzee.inc.php';
     ?>
     <input type="submit" name="reset" value="Reset">
   </form>
