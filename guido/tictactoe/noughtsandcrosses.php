632d1e604aaf7c40f2ead80119ba330998a8ad2c
guidoleijten <guido.leijten@itvitaelearning.nl>
2017-02-26 14:31:36 +0100

    WIP: Tic Tac Toe

--- guido/tictactoe/noughtsandcrosses.php
+++ guido/tictactoe/noughtsandcrosses.php
@@ -6,7 +6,12 @@ if( isset($_SESSION['counter'] ) ) {
    $_SESSION['counter'] = 1;
 }
 //stores anything in $_POST in variable $_SESSION['post-data']
-$_SESSION['post-data'] = $_POST;
+// $_SESSION['post-data'] = $_POST;
+if( isset($_SESSION['post-data'])) {
+   $_SESSION['post-data'] += 1;
+}else {
+   $_SESSION['post-data'] = 1;
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -32,14 +37,20 @@ if (isset($_POST['play'])) {
 }
 
 function playGame() {
-            for ($i=0; $i<9; $i++) {
-                echo '<button name="press" value="'.$i.'">';
-                    if ($_POST['press']==$i)
-                        echo 'X';
-                    echo '</button>';
-                if ($i!=0 && ($i+1)%3==0)
-                    echo "</br>\n";
-                }
+// If 'post-data counter is an even number'
+  if ($_SESSION['post-data'] % 2) {
+    for ($i=0; $i<9; $i++) {
+        echo '<button name="press" value="'.$i.'">';
+            if ($_POST['press']==$i)
+                echo 'X';
+            echo '</button>';
+        if ($i!=0 && ($i+1)%3==0)
+            echo "</br>\n";
+        } elseif () {
+
+        }
+
+  }
 }
 echo "<button id='reset' name='reset'>Play again</button>";
 echo "<br><br>";
