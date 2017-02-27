a2cb887737e3481e9afe7e778cdff1d82962a2c0
AMKuperus <jezeewolde@gmail.com>
2017-02-27 14:36:06 +0100

    StudentActivityTracker
    First setup for StudentActivityTracker Program.

---
+++ AMKuperus/SAT/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<head>
+  <title>StudentActivityTracker</title>
+  <link href="style.css" rel="stylesheet">
+</head>
+<body>
+  <h1>Student Activity Tracker</h1>
+  <!--switch to include login or studentpages or teacherpages-->
+</body>
+</html>
---
+++ AMKuperus/SAT/teacher.inc.php
@@ -0,0 +1,3 @@
+<?php
+##Teacher include
+?>
