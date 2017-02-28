f67e347e8812ddbd2f58669c70993fbf6d4532b6
AMKuperus <jezeewolde@gmail.com>
2017-02-28 21:37:53 +0100

    SAT
    + Added stylesheet
    + Created some layout for login
    + Created basics for login

--- AMKuperus/SAT/index.php
+++ AMKuperus/SAT/index.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 <head>
   <title>StudentActivityTracker</title>
-  <link href="style.css" rel="stylesheet">
+  <link href="style.css" type="text/css" rel="stylesheet">
 </head>
 <body>
   <h1>Student Activity Tracker</h1>
   <!--switch to include login or studentpages or teacherpages-->
+  <?php
+    include 'login.inc.php';
+  ?>
 </body>
 </html>
