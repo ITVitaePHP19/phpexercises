6ebeb1091a8a7461fc917d2273a10a2eb93bdd17
AMKuperus <jezeewolde@gmail.com>
2017-03-01 17:35:13 +0100

    SAT
    + Made include-file for dbsql-functions
    + Changed a little thing in the db-connection-thingy

---
+++ AMKuperus/SAT/functions.inc.php
@@ -0,0 +1,3 @@
+<?php
+//Functions inlude for PDO SQL-functions
+?>
--- AMKuperus/SAT/jumper.inc.php
+++ AMKuperus/SAT/jumper.inc.php
@@ -9,7 +9,8 @@ try {
   //Set PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-  echo 'Database connection error: ' . $e->getMessage();
+  echo 'Database connection error: ' . $e->getMessage() . '<br>';
+  die();
 }
 
 //Close connection $conn = null;
