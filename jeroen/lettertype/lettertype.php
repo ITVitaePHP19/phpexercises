1c5f06b623812ec65d7b63284ef000e601763d69
Jeroen <Jeroendejong1@users.noreply.github.com>
2017-03-01 13:51:31 +0100

    Update lettertype.php

--- jeroen/lettertype/lettertype.php
+++ jeroen/lettertype/lettertype.php
@@ -17,25 +17,21 @@
 <br>
 
 <?php
-//fonts waaruit gekozen kan worden
+//fonts that can be selected
 $fonts = array("Helvetica", "Arial", "Comic Sans", "Tahoma", "Corbel", "Palatino Linotype", "Book Antiqua","Times New Roman","Lucida Sans Unicode", "Lucida Grande");
 
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])){
-	//zet tekst uit bestand om in een string
-	$input = file_get_contents($_POST["file"]);
-}
 
-//zet string om in array
+if (isset($_POST["file"])){$input = file_get_contents($_POST["file"]);}else{$input="";}
+
+//convert string to array
 $array = str_split($input);
 
-//als de array meer dan 4000 tekens bevat, wordt deze daar afgehakt.
+//if the array has more than 4000 characters, the rest of it will not show.
 if (sizeof($array) > 4000){
 	$array=array_slice($array,1,4000);
 	echo "De tekst is te groot en wordt daarom niet volledig weergegeven.<br>";
 }
-
-
-//koppelt alle tekens los, zodat deze individueel een font toegewezen kunnen krijgen 
+//divide all characters. For each one, a random font is selected. 
 foreach ($array as $character){
 	//array rand voor kiezen van een willekeurig font; array_flip om de value terug te geven i.p.v. de key
 	$randomFont = array_rand(array_flip($fonts));
