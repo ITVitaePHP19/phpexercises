3f9941f69a8bad9b4770f43de70de258df775b3c
Jeroen <Jeroendejong1@users.noreply.github.com>
2017-03-01 13:34:42 +0100

    add comments

--- jeroen/wereldvlaggen/bekijken.php
+++ jeroen/wereldvlaggen/bekijken.php
@@ -3,7 +3,6 @@
 <head>
 <title>Bekijken</title>
 <link rel="stylesheet" type="text/css" href="style.css">
-<script src="script.js"></script>
 </head>
 <body>
 <h1>Overzicht</h1>
@@ -11,16 +10,23 @@
 <div class="flex-container">
 
 <?php
-//gets data from json-file
+//gets data from json-file, and put it in $content-variable
 $content = file_get_contents("flags/countries.json");
+
+//decode the $content, and put it in $json-variable
 $json = json_decode($content, true);
 
+//splits data of each country from $json 
 foreach ($json as $total => $country){
+	//splits each value from the key's (e.g. "code" or "name")
 	foreach($country as $key => $value){
+		//if the key is 'code', search inside flags-folder for the flag with the name of the corresponding value,
+		//and echo that flag.
 		if ($key == 'code'){
 				$image = "flags/$value.svg";
 				echo '<div class="flex-item"><img src="'.$image.'" alt="vlag" width="160px" height="100px" />'.PHP_EOL;
-		}		
+		}
+		//if the key is 'name', echo the corresponding value.
 		if($key == 'name'){
 			echo PHP_EOL. $value.'</div>';
 		}
