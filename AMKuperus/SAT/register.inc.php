f35b05fd5572f406bdf6c7aa0bda6e664496028a
AMKuperus <jezeewolde@gmail.com>
2017-03-03 14:42:07 +0100

    SAT
    + Added password fields and funtionality
    ->checks if 2 boxes match
    ->checks if correct mix of  charactyers is present
    -> check if there is atleast 8 characters
    -> creates a hash
    -TODO Still Improve passwordthing with adding errors which should be
    presented back to the user

--- AMKuperus/SAT/register.inc.php
+++ AMKuperus/SAT/register.inc.php
@@ -2,6 +2,9 @@
   <h2>Register</h2>
   <p>Fill in all the fields please</p>
   <p>Username: <input type="text" name="userName" placeholder="username"></p>
+  <small>Password must be minimal 8 characters long and contain a mix of capitals/letters/number/chars.</small>
+  <p>Password: <input type="text" name="pass1" placeholder="********"></p>
+  <p>Re-type password: <input type="password" name="pass2" placeholder="********"></p>
   <p>First name: <input type="text" name="firstName" placeholder="first name"></p>
   <p>Last name: <input type="text" name="lastName" placeholder="last name"></p>
   <p>E-mail: <input type="email" name="email" placeholder="your.name@itvitaelearning.nl"></p>
@@ -15,6 +18,16 @@
     if(isset($_POST['userName'])) {
       $username = filter_input(INPUT_POST, 'userName', FILTER_SANITIZE_STRING);
     } //TODO make elsestatements and  a message when data is not correct or filled in, and then keep correct data in fields
+    if(isset($_POST['pass1'] && isset($_POST['pass2']))) {
+      $p = $_POST['pass1'];
+      if ($p == $_POST['pass2']) {//Both filled in boxes match?
+        if(passContains($p) && strlen($p) >= 8) {//Checking for correct mix and length
+          //Hash the string
+          $pass = passsword_hash($_POST['pass1'], PASSWORD_BCRYPT, ['cost', 8]);
+        }//else password not coorect warning
+      }//else password boxes dont match
+    }//TODO Add error/warningreports in else
+
     if(isset($_POST['firstName'])) {
       $firstName = filter_input(INPUT_POST, 'fisrtName', FILTER_SANITIZE_STRING);
     }
@@ -25,6 +38,20 @@
       $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
     }
     //if(isset($_POST['group'])) {}
+  }
+
+  function passContains($pass) {
+    //Check for small letters a-z
+    $letter = preg_match('/[a-z]/', $pass);
+    //Check for capital letters A-Z
+    $capital = preg_match('/[A-Z]/', $pass);
+    //Check for digits \d
+    $digit = preg_match('/\d/', $pass);
+    //Check for special characters ^a-zA-Z\d (if there is anything else then a-z/A-Z/\d)
+    $special = preg_match('/[^a-zA-Z\d]/', $pass);
+    if($letter && $capital && $digit && $special) {
+      return true;
+    }
   }
 
 ?>
