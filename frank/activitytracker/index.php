f9c6f743df93769bf0edcd88dd404300204074c1
frankgulink <frankgulink@gmail.com>
2017-03-07 13:44:56 +0100

    tic tac toe final

---
+++ frank/activitytracker/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>
+Activiteiten Tracker
+</title>
+<link rel='stylesheet' type='text/css' href='style.css'>  
+</head>
+<body>
+<h1>Activiteiten Tracker</h1> 
+
+<form method='POST'>
+<table>
+<th>MIJN STUDIEONDERDEEL</th>
+<th>BEGINDATUM</th>
+<th>EINDDATUM</th>
+<th>AANTAL BESTEDEN UREN</th>
+<th>% VOLTOOID</th>
+<th>MOEILIJKHEID</th>
+<th>PLEZIER</th>
+<th>NOTITIES</th>
+<tr>
+<td><input type='text'></td>
+<td><input type='date'></td>
+<td><input type='date'></td>
+<td><input type='text'></td>
+<td><progress value="22" max="100"></progress></td>
+<td><input type='number'></td>
+<td><input type='number'></td>
+<td><input type='text'></td>
+</tr>
+</table>  
+</form>
+    
+</body>
+</html>
