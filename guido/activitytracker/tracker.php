b7bcc14d535ad984313b5eb6ec8a9cad8218ba90
guidoleijten <guido.leijten@itvitaelearning.nl>
2017-03-08 14:49:16 +0100

    Update to activity tracker

--- guido/activitytracker/tracker.php
+++ guido/activitytracker/tracker.php
@@ -44,23 +44,25 @@ if (//isset($_POST['id']) &&
     $connection->error . "<br><br>";
 }
 
-//echo <<<_END
-//<form action="tracker.php" method="POST"><pre>
-//My Activities             <input type="text" name="My_Activities">
-//Start Date                <input type="text" name="Start_Date">
-//End Date                  <input type="text" name="End_Date">
-//Time Spent in Hours       <input type="text" name="Time_spent_in_hours">
-//Percentage Completed      <input type="text" name="Percentage_Completed">
-//Pleasure                  <input type="text" name="Pleasure">
-//Difficulty                <input type="text" name="Difficulty">
-//Notes                     <input type="text" name="Notes">
-//                          <input type="submit" value="ADD RECORD">
-//                          </pre></form>
-//_END;
+echo <<<_END
+<link rel="stylesheet" href="stylesheet2.css">
+<form action="tracker.php" method="POST"><pre>
+My Activities             <input type="text" name="My_Activities">
+Start Date                <input type="date" name="Start_Date">
+End Date                  <input type="date" name="End_Date">
+Time Spent in Hours       <input type="text" name="Time_spent_in_hours">
+Percentage Completed      <input type="number" name="Percentage_Completed">
+Pleasure                  <input type="number" name="Pleasure">
+Difficulty                <input type="number" name="Difficulty">
+Notes                     <input type="text" name="Notes">
+                          <input type="submit" value="ADD RECORD">
+                          </pre></form>
+_END;
 
 $query = "SELECT * FROM activitytracker";
 $result = $connection->query($query);
 if (!$result) die ("Database access failed: " . $connection->error);
+//TO DO: Maybe if query entry is succesful, do not show form.
 
 $rows = $result->num_rows;
 
@@ -68,25 +70,50 @@ for ($j = 0; $j < $rows; ++$j) {
   $result ->data_seek($j);
   $row = $result->fetch_array(MYSQLI_NUM);
 
-  echo <<<_END
-<pre>
-My Activities            $row[1]
-Start Date               $row[2]
-End Date                 $row[3]
-Time Spent in Hours      $row[4]
-Percentage Completed     $row[5]
-Pleasure                 $row[6]
-Difficulty               $row[7]
-Notes                    $row[8]
+  $activityHeader = array(
+    "My Activities",
+    "Start Date",
+    "End Date",
+    "Time Spent in Hours",
+    "Percentage Completed",
+    "Pleasure",
+    "Difficulty",
+    "Notes",
+  );
 
-</pre>
+  echo "<table>";
+  foreach ($activityHeader as $head) {
+    echo "<th> $head </th>";
+  }
+//open table row
+  echo "<tr>";
+  //foreach ($row as $table) {
+  //add if statement, if $row = 0, exclude or add from 1+
+  //create new for loop maybe
+  //echo  "<td>".$table."</td>";
+  //}
+  echo "<td>$row[1]</td>";
+  echo "<td>$row[2]</td>";
+  echo "<td>$row[3]</td>";
+  echo "<td>$row[4]</td>";
+  echo "<td>$row[5]</td>";
+  echo "<td>$row[6]</td>";
+  echo "<td>$row[7]</td>";
+  echo "<td>$row[8]</td>";
+
+
+//close table row
+  echo "</tr>";
+  echo "</table>";
+
+echo <<<_END
 <form action="tracker.php" method="POST">
 <input type="hidden" name="delete" value="yes">
-<input type="hidden" name="id" value="$row[0]">
-<input type="submit" value="DELETE RECORD"></form>
+<input type="hidden" name="id" value="$row[0]"><br>
+<input type="submit" value="DELETE RECORD"></form><br>
 _END;
 
-    }
+}
 
 
 
