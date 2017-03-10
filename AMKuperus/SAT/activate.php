340933a954d55e1a1ad0cc74c6245e4cc6f830f3
AMKuperus <jezeewolde@gmail.com>
2017-03-10 22:57:07 +0100

    SAT Activate
    + fixed issue with GET

--- AMKuperus/SAT/activate.php
+++ AMKuperus/SAT/activate.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <?php
   include 'head.inc.php';
-  if(isset($_GET['id'])) {
-    echo 'hello ' . $_GET['id'];
-    if(isset($_GET['t'])) {//TODO grab the token and activate the account, write DB function
-      echo 'token ' . $_GET['t'];
-    }
+  if(isset($_GET['id']) && isset($_GET['t'])) {
+    echo 'hello ' . $_GET['id'] . '<br>';
+    echo 'token ' . $_GET['t'];
+
   }
+//TODO grab the token and activate the account, write DB function
 ?>
