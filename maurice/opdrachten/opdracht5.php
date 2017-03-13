7267cf73ecede2cea9145d777ecba1a2204a6667
Maurice Waney <mjwaney@hotmail.com>
2017-03-13 10:56:18 +0100

    op5
    Wrapped in class, added comments & input validation

--- maurice/opdrachten/opdracht5.php
+++ maurice/opdrachten/opdracht5.php
@@ -4,15 +4,15 @@
 		<table class="midtable">
 			<tr>
 				<td><label for="capital">Capital: </label></td>
-				<td><input type="number" name="capital"></td>
+				<td><input type="text" name="capital"></td>
 			</tr>
 			<tr>
 				<td><label for="interest">Interest: </label></td>
-				<td><input type="number" name="interest"></td>
+				<td><input type="text" name="interest"></td>
 			</tr>
 			<tr>
 				<td><label for="time">Time(in Years): </label></td>
-				<td><input type="number" name="time"></td>
+				<td><input type="text" name="time"></td>
 			</tr>
 			<tr>
 				<td></td>
@@ -24,17 +24,35 @@
 	</form>
 		
 	<?php
+		//Calculate how much you have to pay monthly
+		class MonthlyPayment
+		{
+			private $capital;
+			private $interest;
+			private $iimeInt;
+			
+			//Calculate how much you have to pay monthly
+			function calculatePayment()
+			{
+				if(preg_match("/^[0-9]+$/", $_POST["capital"]) && preg_match("/^[0-9]+$/", $_POST["interest"])
+					&& $_POST["interest"] > 0 && $_POST["interest"] < 100 && preg_match("/^[0-9]+$/", $_POST["time"]))
+				{
+					$this->capital = $_POST["capital"];
+					$this->interest = $_POST["interest"] / 100;
+					$this->timeInt = $_POST["time"] * 12;
+					
+					$pow = pow(1 + $this->interest / 12, $this->timeInt);
+					$monthly = (0.05 / 12 * $pow) / ($pow - 1) * $this->capital;
+					
+					echo "Monthly payment: " . $monthly;
+				} else{ echo "Not a valid number.";}
+			}
+		}
+		$mP = new MonthlyPayment;
 		
 		if( isset($_POST["submit"]) ) 
 		{
-			$capital = $_POST["capital"];
-			$interest = $_POST["interest"] / 100;
-			$time = $_POST["time"] * 12;
-			
-			$pow = pow(1 + $interest / 12, $time);
-			$monthly = (0.05 / 12 * $pow) / ($pow - 1) * $capital;
-			
-			echo "Monthly payment: " . $monthly;
+			$mP->calculatePayment();
 		}	
 	?>
 </article>
