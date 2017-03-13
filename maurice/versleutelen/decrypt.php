089f65fa8d2103ca982c2df312fec28281dc059c
Maurice Waney <mjwaney@hotmail.com>
2017-03-13 13:01:02 +0100

    decrypt
    wrapped in class

--- maurice/versleutelen/decrypt.php
+++ maurice/versleutelen/decrypt.php
@@ -1,5 +1,12 @@
 <?php
-	$enc = $_SESSION["key"];
-	 
-	echo $enc;
+	class Decryption
+	{
+		function decrypt()
+		{
+			$enc = $_SESSION["key"];
+		 
+			echo $enc;
+		}
+	}
+	$dc = new Decryption;
 ?>
