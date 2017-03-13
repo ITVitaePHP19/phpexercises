345714e71757821f2f9bcb0d6cf8bbd508bb84b2
Maurice Waney <mjwaney@hotmail.com>
2017-03-13 10:59:59 +0100

    temp
    Wrapped in class, added comments & input validation

--- maurice/opdrachten/temperature.php
+++ maurice/opdrachten/temperature.php
@@ -1,15 +1,31 @@
 <?php
-	$convert = $_POST["temp"];
-	$input = $_POST["input"];
+	//converts fahrenheit to celsius or vice versa
+	class TemperatureConverter
+	{
+		private $convert;
+		private $input;
+		
+		//converts fahrenheit to celsius or vice versa
+		function convert()
+		{
+			if(preg_match("/^[0-9]+$/", $_POST["input"]))
+			{
+				$this->convert = $_POST["temp"];
+				$this->input = $_POST["input"];
+				
+				if($this->convert == "fahrenheit"){
+					$result = ($this->input - 32) * 5/9;
+					$other = "celsius";
+				}
+				else{
+					$result = $this->input * 9/5 + 32;	;
+					$other = "fahrenheit";
+				}
+				echo $_POST["input"] . " " . $this->convert . " is " . $result . " " . $other;
+			} else{ echo "Not a valid number.";}
+		}
+	}
+	$tC = new TemperatureConverter;
 	
-	if($convert == "fahrenheit"){
-		$result = ($input - 32) * 5/9;
-		$other = "celsius";
-	}
-	else{
-		$result = $input * 9/5 + 32;	;
-		$other = "fahrenheit";
-	}
-	echo $_POST["input"] . " " . $convert . " is " . $result . " " . $other;
 
 ?>
