90a25135bda0637555d340edbfc4c83ca5ce9219
Maurice Waney <mjwaney@hotmail.com>
2017-03-13 11:01:21 +0100

    stickheight
    Wrapped in class, added comments & input validation

--- maurice/opdrachten/stickheight.php
+++ maurice/opdrachten/stickheight.php
@@ -1,15 +1,31 @@
 <?php
-	$input = $_POST["input"];
-	$value = $_POST["walk"];
-	
-	if($value == "ccc"){
-		$result = $input * 0.9;
+	//calculates the height your stick should be depending on your height and usage
+	class StickHeight
+	{
+		private $input;
+		private $value;
+		
+		//calculates the height your stick should be depending on your height and usage
+		function calculateHeight()
+		{
+			if(preg_match("/^[0-9]+$/", $_POST["input"]) && $_POST["input"] > 100 && $_POST["input"] < 250)
+			{
+				$this->input = $_POST["input"];
+				$this->value = $_POST["walk"];
+				
+				if($this->value == "ccc"){
+					$result = $this->input * 0.9;
+				}
+				elseif($this->value == "ccfs"){
+					$result = $this->input * 0.85;
+				}
+				else{
+					$result = $this->input * 0.68;	
+				}
+				echo "Height of the stick should be about " . $result . " centimeters";
+			} else{ echo "Not a valid number.";}
+		}
 	}
-	elseif($value == "ccfs"){
-		$result = $input * 0.85;
-	}
-	else{
-		$result = $input * 0.68;	
-	}
-	echo "Height of the stick should be about " . $result . " centimeters";
+	$sH = new StickHeight;
+
 ?>
