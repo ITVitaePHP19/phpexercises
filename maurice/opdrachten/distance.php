d5a7e31bb070e70874df7d290e705d78d40560cd
Maurice Waney <mjwaney@hotmail.com>
2017-03-13 10:58:54 +0100

    distance
    Wrapped in class, added comments & input validation

--- maurice/opdrachten/distance.php
+++ maurice/opdrachten/distance.php
@@ -1,14 +1,31 @@
 <?php
-	$convert = $_POST["distance"];
-	$input = $_POST["input"];
+	//converts miles to kilometres or vice versa
+	class ConvertDistance
+	{
+		private $convert;
+		private $input;
+		
+		
+		//converts miles to kilometres or vice versa
+		function convert()
+		{
+			if(preg_match("/^-?(?:\d+|\d*\.\d+)$/", $_POST["input"]))
+			{
+				$this->convert = $_POST["distance"];
+				$this->input = $_POST["input"];
+				if($this->convert == "kilometres"){
+					$this->input *= 0.621371192;
+					$other = "miles";
+				}
+				else{
+					$this->input *= 1.609344;
+					$other = "kilometres";
+				}
+				echo $_POST["input"] . " " . $this->convert . " is " . $this->input . " " . $other;
+			} else{ echo "Not a valid number.";}
+		}
+	}
+	$cD = new ConvertDistance;
 	
-	if($convert == "kilometres"){
-		$input *= 0.621371192;
-		$other = "miles";
-	}
-	else{
-		$input *= 1.609344;
-		$other = "kilometres";
-	}
-	echo $_POST["input"] . " " . $convert . " is " . $input . " " . $other;
+	
 ?>
