1062ce7b834fb0080f07c9a9553b15b9a6d120fd
Maurice Waney <mjwaney@hotmail.com>
2017-03-13 11:31:18 +0100

    calculator/result.php
    wrapped in class

--- maurice/calculator/result.php
+++ maurice/calculator/result.php
@@ -1,37 +1,46 @@
 <?php		
 	require_once("memory.php");
 	
-	if(isset($_POST["press"]))
+	//calculates the result of the input
+	class CalculateResult
 	{
-		
-		$percentage = "/100 *";
-			
-		//clear
-		if($_POST["press"] == 'C')
+		//gets button presses and calculates math stuff
+		function calc()
 		{
-			$_SESSION["save"] = "";
+			if(isset($_POST["press"]))
+			{
+				
+				$percentage = "/100 *";
+					
+				//clear
+				if($_POST["press"] == 'C')
+				{
+					$_SESSION["save"] = "";
+				}
+				else
+				{
+					//store input in $result string concatenated with the session variable
+					$result = $_SESSION["save"] .= $_POST["press"];
+					$old = array("M+", "M-", "MR", "MC");
+					$new = array("", "", "", "");
+					$result = str_replace($old, $new, $result);
+					echo $result . " ";
+					
+					//replace string operators with their php counterparts
+					$operators = array("x", "=", "%");
+					$replace = array("*", "", $percentage, "");		
+					$calc = str_replace($operators, $replace, $result);
+				}
+				
+				//echo results with '='
+				if($_POST["press"] == '=')
+				{
+					$equals = eval("return " . "$calc;");
+					echo $equals;
+					$_SESSION["save"] = $equals;
+				}
+			} else{ $_SESSION["save"] = ""; }
 		}
-		else
-		{
-			//store input in $result string concatenated with the session variable
-			$result = $_SESSION["save"] .= $_POST["press"];
-			$old = array("M+", "M-", "MR", "MC");
-			$new = array("", "", "", "");
-			$result = str_replace($old, $new, $result);
-			echo $result . " ";
-			
-			//replace string operators with their php counterparts
-			$operators = array("x", "=", "%");
-			$replace = array("*", "", $percentage, "");		
-			$calc = str_replace($operators, $replace, $result);
-		}
-		
-		//echo results with '='
-		if($_POST["press"] == '=')
-		{
-			$equals = eval("return " . "$calc;");
-			echo $equals;
-			$_SESSION["save"] = $equals;
-		}
-	} else{ $_SESSION["save"] = ""; }
+	}
+	$cR = new CalculateResult;
 ?>
