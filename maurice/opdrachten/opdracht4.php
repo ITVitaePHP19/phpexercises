d756d657fca376c122a5f78d6e9adbc9a1437aa5
Maurice Waney <mjwaney@hotmail.com>
2017-03-13 10:55:40 +0100

    op4
    Wrapped in class, added comments & input validation

--- maurice/opdrachten/opdracht4.php
+++ maurice/opdrachten/opdracht4.php
@@ -4,11 +4,11 @@
 		<table class="midtable">
 			<tr>
 				<td><label for="gross">Gross Income: </label></td>
-				<td><input type="number" name="gross"></td>
+				<td><input type="text" name="gross" required></td>
 			</tr>
 			<tr>
 				<td><label for="tax">Tax Percentage: </label></td>
-				<td><input type="number" name="tax"></td>
+				<td><input type="text" name="tax"required></td>
 			</tr>
 			
 			<tr>
@@ -21,18 +21,34 @@
 	</form>
 		
 	<?php
+		//Calculates net salary
+		class TaxPercentage
+		{
+			private $gross;
+			private $pension = 0.043;
+			
+			//checks for valid input and calculates net income by subtracting tax
+			function calculateTax()
+			{
+				if(preg_match("/^[0-9]+$/", $_POST["gross"]) && preg_match("/^[0-9]+$/", $_POST["tax"])
+					&& $_POST["tax"] > 0 && $_POST["tax"] < 100)
+				{
+					$this->gross = $_POST["gross"];
+					$tax = $_POST["tax"] / 100;
+					
+					$grosstax = $this->gross * $tax;
+					$grosspension = $this->gross * $this->pension;
+					$net = $this->gross - ($grosstax + $grosspension);
+					
+					echo "Salary: " . $net;
+				} else{ echo "Not a valid number.";}
+			}
+		}
+		$tP = new TaxPercentage;
 		
 		if( isset($_POST["submit"]) ) 
 		{
-			$gross = $_POST["gross"];
-			$tax = $_POST["tax"] / 100;
-			$pension = 0.043;
-			
-			$grosstax = $gross * $tax;
-			$grosspension = $gross * $pension;
-			$net = $gross - ($grosstax + $grosspension);
-			
-			echo "Salary: " . $net;
+			$tP->calculateTax();			
 		}	
 	?>
 </article>
