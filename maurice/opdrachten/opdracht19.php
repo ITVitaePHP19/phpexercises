77b48dbd1f3cda955671f6aeb53755924a54256d
Maurice Waney <mjwaney@hotmail.com>
2017-03-13 11:04:31 +0100

    op19
    Wrapped in class, added comments & input validation

--- maurice/opdrachten/opdracht19.php
+++ maurice/opdrachten/opdracht19.php
@@ -4,28 +4,39 @@
 </form>
 
 <?php
-
-function luhn($num){
-	if(!$num)
-		return false;
-	$num = array_reverse(str_split($num));
-	$add = 0;
-	foreach($num as $k => $v){
-		if($k%2)
-			$v = $v*2;
-		$add += ($v >= 10 ? $v - 9 : $v);
+	//checks if the number you entered is a valid SIM-card number
+	class Luhn
+	{
+		//takes the input as an argument and returns 1 if it is a valid SIM-card number
+		function calculateLuhn($num)
+		{
+			if(!$num)
+				return false;
+			$num = array_reverse(str_split($num));
+			$add = 0;
+			foreach($num as $k => $v){
+				if($k%2)
+					$v = $v*2;
+				$add += ($v >= 10 ? $v - 9 : $v);
+			}
+			return ($add%10 == 0); 
+		}
 	}
-	return ($add%10 == 0); 
-}
+	$ln = new Luhn;
 
 if(isset($_POST["submit"]))
 {
-	$luhn = $_POST["luhn"];
+	$input = $_POST["luhn"];
 	
-	$result = luhn($luhn);
+	$result = $ln->calculateLuhn($input);
+	
 	if($result == 1)
 	{
 		echo "This is a correct SIM-card number";
-	}else { echo "This is NOT a valid SIM-card number"; }
+	}
+	else 
+	{ 
+		echo "This is NOT a valid SIM-card number"; 
+	}
 }
 ?>
