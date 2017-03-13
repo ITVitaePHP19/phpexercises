c2b650476b150389efb3cf96c2b76c53f26a77e9
Maurice Waney <mjwaney@hotmail.com>
2017-03-13 11:02:24 +0100

    housingallowance
    Wrapped in class, added comments & input validation

--- maurice/opdrachten/housingallowance.php
+++ maurice/opdrachten/housingallowance.php
@@ -1,18 +1,33 @@
 <?php
-	$cost = $_POST["cost"];
-	$upper = 33.64252;
-	$lower = 33.64000;
-	$allowance;
-	
-	if($cost < $lower)
+	//calculates allowance depending on your cost?
+	class HousingAllowance
 	{
-		$allowance = 0;
+		private $cost;
+		private $allowance;
+		
+		//calculates allowance depending on your cost?
+		function calculateAllowance()
+		{
+			if(preg_match("/^-?(?:\d+|\d*\.\d+)$/", $_POST["cost"]))
+			{
+				$this->cost = $_POST["cost"];
+				$upper = 33.64252;
+				$lower = 33.64000;
+				$this->allowance;
+				
+				if($this->cost < $lower)
+				{
+					$this->allowance = 0;
+				}
+				elseif($this->cost > $upper)
+				{
+					$this->allowance = 201.60;
+				}
+				else $this->allowance = $this->cost * .80;
+				
+				echo $this->allowance;
+			} else{ echo "Not a valid number.";}
+		}
 	}
-	elseif($cost > $upper)
-	{
-		$allowance = 201.60;
-	}
-	else $allowance = $cost * .80;
-	
-	echo $allowance;
+	$hA = new HousingAllowance;
 ?>
