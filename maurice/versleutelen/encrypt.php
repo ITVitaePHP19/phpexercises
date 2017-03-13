564bd0495699568ce1bbf56eedad1fea079cd7ec
Maurice Waney <mjwaney@hotmail.com>
2017-03-13 13:00:45 +0100

    encrypt
    wrapped in class

--- maurice/versleutelen/encrypt.php
+++ maurice/versleutelen/encrypt.php
@@ -1,29 +1,31 @@
 <?php
 	// phpinfo();
-	function encrypt()
-	{	
-		if(isset($_POST["fileToUpload"]))
-		{
-			$file = $_POST["fileToUpload"];
-			$enc = file_get_contents($file); 
-		}	
-		if(isset($_POST["enctext"]))
-		{
-			$enc = $_POST["enctext"];
-		}	
-		
-		$_SESSION["key"] = $enc;
-		
-		$enc = str_split($enc);
-		$characters = str_split("abcdefghijklmnopqrstuvwxyz1234567890-=[]{};'./,<>:");
-		shuffle($characters);
-		
-		for($i = 0; $i < count($enc); $i++)
-		{
-			$enc[$i] = $characters[rand(0, count($characters) - 1)];
+	class Encryption
+	{
+		function encrypt()
+		{	
+			if(isset($_POST["fileToUpload"]))
+			{
+				$file = $_POST["fileToUpload"];
+				$enc = file_get_contents($file); 
+			}	
+			if(isset($_POST["enctext"]))
+			{
+				$enc = $_POST["enctext"];
+			}	
+			
+			$_SESSION["key"] = $enc;
+			
+			$enc = str_split($enc);
+			$characters = str_split("abcdefghijklmnopqrstuvwxyz1234567890-=[]{};'./,<>:");
+			shuffle($characters);
+			
+			for($i = 0; $i < count($enc); $i++)
+			{
+				$enc[$i] = $characters[rand(0, count($characters) - 1)];
+			}
+			echo implode("", $enc);
 		}
-		echo implode("", $enc);
 	}
-	
-	encrypt();
+	$enc = new Encryption;
 ?>
