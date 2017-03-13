c6178b14aa8569d29cf08654c7419275f3e7b817
Maurice Waney <mjwaney@hotmail.com>
2017-03-13 10:53:36 +0100

    op2
    Wrapped in class, added comments, and input validation

--- maurice/opdrachten/opdracht2.php
+++ maurice/opdrachten/opdracht2.php
@@ -4,11 +4,11 @@
 		<table class="midtable">
 			<tr>
 				<td><label for="height">Height in meters: </label></td>
-				<td><input type="text" name="height"></td>
+				<td><input type="text" name="height" required></td>
 			</tr>
 			<tr>
 				<td><label for="weight">Weight in kilograms: </label></td>
-				<td><input type="text" name="weight"></td>
+				<td><input type="text" name="weight" required></td>
 			</tr>
 			<tr>
 				<td></td>
@@ -22,14 +22,32 @@
 		
 	<?php
 		
+		//body mass index calculator
+		class BMI
+		{
+			private $height;
+			private $weight;
+			
+			//check if height and weight input are valid values and if so calculates bmi
+			public function calculateBMI()
+			{
+				if(preg_match("/^-?(?:\d+|\d*\.\d+)$/", $_POST["height"]) && preg_match("/^[0-9]+$/", $_POST["weight"]) &&
+					$_POST["weight"] < 800 && $_POST["weight"] > 20 && $_POST["height"] > 1.00 && $_POST["height"] < 2.50)
+				{
+					$this->height = $_POST["height"];
+					$this->weight = $_POST["weight"];
+					
+					$bmi = $this->weight / ($this->height * $this->height);
+				
+					echo $bmi;
+				} else { echo "Not a valid height and/or weight input.";}
+			}
+		}
+		$bmi = new BMI;
+		
 		if( isset($_POST["submit"]) ) 
 		{
-			$height = $_POST["height"];
-			$weight= $_POST["weight"];
-			
-			$bmi = $weight / ($height * $height);
-			
-			echo $bmi;
+			$bmi->calculateBMI();	
 		}	
 	?>
 </article>
