fdaec6b9eef30683b9fb6e20be4e4728eb63c55d
Maurice Waney <mjwaney@hotmail.com>
2017-03-13 11:03:30 +0100

    op12
    Wrapped in class, added comments & input validation

--- maurice/opdrachten/opdracht12.php
+++ maurice/opdrachten/opdracht12.php
@@ -1,9 +1,21 @@
 <br>
 <table border="1">
 	<?php
-		for($i = 8; $i < 17; $i++)
+	
+		//echos the time by the hour from 8:00 to 16:00
+		class TimeTable
 		{
-			echo "<tr><td>" . $i . ":00</td></tr><br>";
+			//echos the time by the hour from 8:00 to 16:00
+			function drawTT()
+			{
+				for($i = 8; $i < 17; $i++)
+				{
+					echo "<tr><td>" . $i . ":00</td></tr><br>";
+				}
+			}
 		}
+		$tT = new TimeTable;
+		
+		$tT->drawTT();
 	?>
 </table>
