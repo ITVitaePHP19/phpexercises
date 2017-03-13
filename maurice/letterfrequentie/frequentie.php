6bdb4af6d59aae6bb7bfcc6a1c00fef6ff547b3c
Maurice Waney <mjwaney@hotmail.com>
2017-03-13 13:09:39 +0100

    frequentie.php
    wrapped in class

--- maurice/letterfrequentie/frequentie.php
+++ maurice/letterfrequentie/frequentie.php
@@ -1,11 +1,17 @@
 <?php
-	
-	$string = strtolower($_POST["frequentie"]);
-	
-	$letters = str_split("abcdefghijklmnopqrstuvwxyz");
-	
-	for($i = 0; $i < count($letters); $i++)
+	class LetterFrequentie
 	{
-		echo $letters[$i] . ": " . substr_count($string, $letters[$i]) . " ";
+		function calcFrequentie()
+		{
+			$string = strtolower($_POST["frequentie"]);
+			
+			$letters = str_split("abcdefghijklmnopqrstuvwxyz");
+			
+			for($i = 0; $i < count($letters); $i++)
+			{
+				echo $letters[$i] . ": " . substr_count($string, $letters[$i]) . " ";
+			}
+		}	
 	}
+	$lF = new LetterFrequentie;
 ?>
