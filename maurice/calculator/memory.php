b25aebc04363c907f403497d1da8506c6efd4a86
Maurice Waney <mjwaney@hotmail.com>
2017-03-13 11:33:23 +0100

    calculator/memory
    wrapped in class

--- maurice/calculator/memory.php
+++ maurice/calculator/memory.php
@@ -1,23 +1,31 @@
 <?php
-	
-	if(isset($_POST["press"]))
+	//manages calculator memory
+	class CalculatorMemory
 	{
-		switch($_POST["press"])
+		//gets button events and does memory calculations and stuff
+		function memory()
 		{
-			case 'M+':
-				$_SESSION["memory"] += $_SESSION["save"];
-				break;
-			case 'M-':
-				$_SESSION["memory"] -= $_SESSION["save"];
-				break;
-			case 'MR':
-				$_SESSION["save"] .= $_SESSION["memory"];
-				break;
-			case 'MC':
-				$_SESSION["memory"] = "";
-				break;	
+			if(isset($_POST["press"]))
+			{
+				switch($_POST["press"])
+				{
+					case 'M+':
+						$_SESSION["memory"] += $_SESSION["save"];
+						break;
+					case 'M-':
+						$_SESSION["memory"] -= $_SESSION["save"];
+						break;
+					case 'MR':
+						$_SESSION["save"] .= $_SESSION["memory"];
+						break;
+					case 'MC':
+						$_SESSION["memory"] = "";
+						break;	
+				}
+			}else{ $_SESSION["memory"] = ""; }
+			
+			echo $_SESSION["memory"];
 		}
-	}else{ $_SESSION["memory"] = ""; }
-	
-	echo $_SESSION["memory"];
+	}
+	$cM = new CalculatorMemory;
 ?>
