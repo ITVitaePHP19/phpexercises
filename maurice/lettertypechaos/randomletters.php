ce1b71c36c1a9bc6b1926e50ae9f11a5b198603c
Maurice Waney <mjwaney@hotmail.com>
2017-03-13 13:55:28 +0100

    randomletters.php
    wrapped in class

--- maurice/lettertypechaos/randomletters.php
+++ maurice/lettertypechaos/randomletters.php
@@ -1,19 +1,30 @@
 <?php
-	$text = str_split($_POST["textchaos"]);
-
-	$one = "class='one'";
-	$two = "class='two'";
-	$three = "class='three'";
-	$four = "class='four'";
-	$five = "class='five'";
-	$six = "class='six'";
-	$seven = "class='seven'";
-	$eight = "class='eight'";
-	
-	$styleArray = array($one, $two, $three, $four, $five, $six, $seven, $eight);
-		
-	for($i = 0; $i < count($text); $i++)
+	class LettertypeChaos
 	{
-		echo "<b " . $styleArray[rand(0, 7)] . "'>" . strtoupper($text[$i]) . "</b>";
+		function randomize()
+		{
+			$text = str_split($_POST["textchaos"]);
+
+			$one = "class='one'";
+			$two = "class='two'";
+			$three = "class='three'";
+			$four = "class='four'";
+			$five = "class='five'";
+			$six = "class='six'";
+			$seven = "class='seven'";
+			$eight = "class='eight'";
+			
+			$colorArray = array("red", "blue", "yellow", "green", "purple", "pink", "orange", "black");
+			$styleArray = array($one, $two, $three, $four, $five, $six, $seven, $eight);
+			echo "<div style='background-color:" . $colorArray[rand(0, 7)] . "'>";	
+			
+			for($i = 0; $i < count($text); $i++)
+			{
+				echo "<b " . $styleArray[rand(0, 7)] . "'>" . strtoupper($text[$i]) . "</b>";
+			}
+			
+			echo "</div>";	
+		}
 	}
+	$lC = new LettertypeChaos;
 ?>
