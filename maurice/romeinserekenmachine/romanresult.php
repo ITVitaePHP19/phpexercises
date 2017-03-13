9b73f3967e88100dc3aaa4e7ff610e21c9e7fdfc
Maurice Waney <mjwaney@hotmail.com>
2017-03-13 13:52:03 +0100

    romanresult.php
    - fixed bug where first input isn't recognized in certain browsers and
    incognito mode
    - wrapped in class

--- maurice/romeinserekenmachine/romanresult.php
+++ maurice/romeinserekenmachine/romanresult.php
@@ -1,50 +1,84 @@
-<?php	
-	require("calculateRoman.php");
-	// phpinfo();
-	
-	// 1. If key is pressed
-	if(isset($_POST["press"]))
+<?php
+	class RomanInput
 	{
-		$result;
-		
-		//string session
-		if(!isset($_SESSION["saveRoman"]))
+		function inputProcessing()
 		{
-			$_SESSION["saveRoman"] = "";
+			require("calculateRoman.php");
+			// phpinfo();
+			
+			//string session
+			if(!isset($_SESSION["saveRoman"]))
+			{
+				$_SESSION["saveRoman"] = "";
+			}
+			
+			// 1. If key is pressed
+			if(isset($_POST["press"]))
+			{
+				$result = "";
+				$operator = array("+", "-", "*", "/");
+				$equals = "=";
+				
+				//clear if pressed
+				if($_POST["press"] == "CL")
+				{
+					$_SESSION["saveRoman"] = "";
+					echo $_SESSION["saveRoman"];
+				}
+				// 2. else concatenate the strings for display
+				else
+				{
+					$result = $_SESSION["saveRoman"] .= $_POST["press"];
+										
+					//check if first input isn't an operator or equals sign
+					if(in_array(substr($result, 0, 1), $operator) || substr($result, 0, 1) == "=")
+					{
+						$result = str_replace($result, $result, "");
+						$_SESSION["saveRoman"] = $result;
+					}
+					//if there's two operators in succession remove the last one 
+					if(in_array(substr($result, -2, 1), $operator) && in_array(substr($result, -1, 1), $operator))
+					{
+						$result = substr($result, 0, -1);
+						$_SESSION["saveRoman"] = $result;
+						echo $result . "";
+					}
+					else
+					{
+						echo $result . "";
+					}
+					
+				}
+				
+				//calculate everything when equals sign is pressed
+				if(	strpos($result, '=') !== false && (strpos($result, "+") !== false || strpos($result, "-") !== false || 
+					strpos($result, "/") !== false || strpos($result, "*") !== false) )
+				{
+					$parts = preg_split("/([\*\/\-\+\=])/", $result, -1, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY);
+					
+					$number1 = $parts[0];
+					$operator = $parts[1];
+					$number2 = $parts[2];
+					
+					$n1 = $rC->normalize($number1);
+					$n2 = $rC->normalize($number2);
+					
+					$calc = $n1 . $operator . $n2;
+					
+					$normalResult = eval("return " . "$calc;");
+					
+					$romanResult = $rC->romanize($normalResult);
+					echo $romanResult;
+					
+					// var_dump($calc . " = " . $normalResult);
+				}
+				elseif(strpos($result, '=') !== false)
+				{
+					$result = substr($result, 0, -1);
+					$_SESSION["saveRoman"] = $result;
+				}
+			}
 		}
-		//clear if pressed
-		elseif($_POST["press"] == "CL")
-		{
-			$_SESSION["saveRoman"] = "";
-			echo $_SESSION["saveRoman"];
-		}
-		// 2. else concatenate the strings for display
-		else
-		{
-			$result = $_SESSION["saveRoman"] .= $_POST["press"];
-			echo $result . "";
-		}	
-		
-		//calculate everything when equals sign is pressed
-		if($_POST["press"] == "=")
-		{
-			$parts = preg_split("/([\*\/\-\+\=])/", $result, -1, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY);
-			
-			$number1 = $parts[0];
-			$number2 = $parts[2];
-			$operator = $parts[1];
-			
-			$n1 = normalize($number1);
-			$n2 = normalize($number2);
-			
-			$calc = $n1 . $operator . $n2;
-			
-			$normalResult = eval("return " . "$calc;");
-			
-			$romanResult = romanize($normalResult);
-			echo $romanResult;
-			
-			// var_dump($calc . " = " . $normalResult);
-		}			
 	}
+	$rI = new RomanInput;
 ?>
