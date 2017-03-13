b593936966928f11f3756daa9bab9439f3b62a53
Maurice Waney <mjwaney@hotmail.com>
2017-03-13 11:04:01 +0100

    op13
    Wrapped in class, added comments & input validation

--- maurice/opdrachten/opdracht13.php
+++ maurice/opdrachten/opdracht13.php
@@ -1,13 +1,26 @@
 <br>
 <table border="1">
-	<?php		
-		for($i = 8; $i < 17; $i++)
+	<?php
+		//echos the time by the hour from 8:00 to 16:00 plus the working days of the week
+		class TimeTableDays
 		{
-			if($i == 8)
+			
+			//echos the time by the hour from 8:00 to 16:00 plus the working days of the week
+			function drawTable()
 			{
-				echo "<tr><td></td><td>Monday</td><td>Tuesday</td><td>Wednesday</td><td>Thursday</td><td>Friday</td></td>";
+				for($i = 8; $i < 17; $i++)
+				{
+					if($i == 8)
+					{
+						echo "<tr><td></td><td>Monday</td><td>Tuesday</td><td>Wednesday</td><td>Thursday</td><td>Friday</td></td>";
+					}
+					echo "<tr><td>" . $i . ":00</td><td></td><td></td><td></td><td></td><td></td></tr><br>";
+				}	
 			}
-			echo "<tr><td>" . $i . ":00</td><td></td><td></td><td></td><td></td><td></td></tr><br>";
 		}
+		$tTD = new TimeTableDays;
+		
+		$tTD->drawTable();
+		
 	?>
 </table>
