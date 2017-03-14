29349fcd20c72b5b1a127ae20d103029f4114455
Maurice Waney <mjwaney@hotmail.com>
2017-03-14 10:05:28 +0100

    answers.php
    wrapped in class

--- maurice/vlaggen/answers.php
+++ maurice/vlaggen/answers.php
@@ -1,11 +1,18 @@
 <?php
-	$rightanswer = $_SESSION["rightanswer"];
-	$answer = $_POST["answer"];
-						//cwcw
-		if($answer == $rightanswer){
-			echo "<br>" . $answer . " is correct!<br><br>";
+	class Answers
+	{
+		function getAnswer()
+		{
+			$rightanswer = $_SESSION["rightanswer"];
+			$answer = $_POST["answer"];
+							//cwcw
+			if($answer == $rightanswer){
+				echo "<br>" . $answer . " is correct!<br><br>";
+			}
+			else{
+				echo "<br>Incorrect. The right answer is: " . $rightanswer . "<br><br>";
+			}
 		}
-		else{
-			echo "<br>Incorrect. The right answer is: " . $rightanswer . "<br><br>";
-		}
+	}
+	$a = new Answers;
 ?>
