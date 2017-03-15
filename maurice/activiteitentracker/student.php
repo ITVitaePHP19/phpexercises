a2e2c5701a6cbc7d1dafa650c92a9603f408f04d
Maurice Waney <mjwaney@hotmail.com>
2017-03-15 10:33:59 +0100

    student.php
    fixed redirect

--- maurice/activiteitentracker/student.php
+++ maurice/activiteitentracker/student.php
@@ -1,5 +1,23 @@
 <?php 
-		include_once "displayRecords.php";
-		displayRow($email);
-		addRow();
+	if(!isset($_SESSION)) {
+	   session_start();
+	}
 ?>
+<html>
+<?php 
+	class Student
+	{
+		
+		function displayStudent()
+		{
+			$email = $_SESSION["email"];
+			include_once "displayRecords.php";
+			$e->export();
+			$dR->displayRow($email);
+			$dR->addRow();
+		}
+	}
+	$s = new Student;
+	$s->displayStudent();
+?>
+</html>
